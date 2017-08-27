7bbd811ed3df451ff15de15d890a2626bfcc88af
Joseph kadiri <kadiri.joseph@allianz.com>
2017-08-27 17:02:00 +0700

    modified condition on delete transaction

--- src/core/transaction/transaction.delete.js
+++ src/core/transaction/transaction.delete.js
@@ -5,44 +5,41 @@
 
 function transactionDelete(query)
 {
-    var $self = this;
-    if(query)
+    var $self = this,
+        delItem = [];
+    new $query(this.tableInfo.data)._(query,function(item,idx){
+      delItem.push(item);
+    });
+
+    this.executeState.push(["delete",function(disableOfflineCache)
     {
-      var delItem = [];
-      new $query(this.tableInfo.data)._(query,function(item,idx){
-        delItem.push(item);
-      });
+        if(!delItem.length)
+        {
+          throw Error(["No record(s) to remove"]);
+        }else
+        {
+            $self.tableInfo.data = $self.tableInfo.data.filter(function(item)
+            {
+              return !$inArray(item,delItem);
+            });
 
-      this.executeState.push(["delete",function(disableOfflineCache)
-      {
-          if(!delItem.length)
-          {
-            throw Error(["No record(s) to remove"]);
-          }else
-          {
-              $self.tableInfo.data = $self.tableInfo.data.filter(function(item)
-              {
-                return !$inArray(item,delItem);
+
+              //push records to our resolver
+              if(!disableOfflineCache){
+                $self.updateOfflineCache('delete',delItem);
+              }
+
+              /**
+                  broadcast event
+              **/
+              $queryDB.storageEventHandler.broadcast(eventNamingIndex($self.tableInfo.DB_NAME,'delete'), [$self.tableInfo.TBL_NAME, delItem]);
+
+            //return success Message
+            return ({
+                message : delItem.length + " record(s) removed"
               });
-
-
-                //push records to our resolver
-                if(!disableOfflineCache){
-                  $self.updateOfflineCache('delete',delItem);
-                }
-
-                /**
-                    broadcast event
-                **/
-                $queryDB.storageEventHandler.broadcast(eventNamingIndex($self.tableInfo.DB_NAME,'delete'), [$self.tableInfo.TBL_NAME, delItem]);
-
-              //return success Message
-              return ({
-                  message : delItem.length + " record(s) removed"
-                });
-          }
-        }]);
-      }
+        }
+      }]);
 
       return this;
 };
