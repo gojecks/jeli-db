eed0e9b43a98667d6e830753d5c97b2cad52ea08
Joseph Gojecks <gojeckskenwood@gmail.com>
2018-02-24 16:38:23 +0700

    added new facade to resolvers and resource mangers instance

--- src/core/opendb.resolvers.core.js
+++ src/core/opendb.resolvers.core.js
@@ -5,7 +5,17 @@ function openedDBResolvers() {
     this.networkResolver = ({
         serviceHost: null,
         dirtyCheker: true,
-        conflictResolver: null,
+        conflictResolver: function(response, currentProcessTbl, mergeConflictResolver, failedConflictResolver) {
+            if (confirm('Update your table(' + currentProcessTbl + ') with Server records (yes/no)')) {
+                syncHelper.setMessage('Updating Local(' + currentProcessTbl + ') with Server(' + currentProcessTbl + ')', this);
+                mergeConflictResolver(response.conflictRecord, currentProcessTbl);
+            } else {
+                failedConflictResolver();
+            }
+        },
+        resolveDeletedTable: function(currentProcessTbl) {
+            return (confirm('Are you sure you want to drop table ' + currentProcessTbl));
+        },
         logger: [],
         logService: function() {},
         interceptor: function() {},
@@ -23,6 +33,11 @@ function openedDBResolvers() {
         ignoreSync: []
     });
 
+    /**
+     * 
+     * @param {*} name 
+     * @param {*} value 
+     */
     this.register = function(name, value) {
         if ($isObject(name) && !value) {
             this.networkResolver = extend(this.networkResolver, name);
@@ -33,7 +48,62 @@ function openedDBResolvers() {
         return this;
     };
 
+    /**
+     * 
+     * @param {*} name 
+     */
     this.getResolvers = function(name) {
         return this.networkResolver[name] || '';
+    };
+};
+
+/**
+ * 
+ * @param {*} name 
+ */
+openedDBResolvers.$hasOwnPropery = function(name) {
+    return this.networkResolver.hasOwnProperty(name);
+};
+
+
+/**
+ * 
+ * @param {*} dbName 
+ */
+openedDBResolvers.prototype.deleteManager = function(dbName) {
+    var _self = this;
+    return {
+        init: function() {
+            var $delRecords = getStorageItem($queryDB.$delRecordName);
+            if ($delRecords && $delRecords.hasOwnProperty(dbName)) {
+                //update deleted records
+                _self
+                    .register('deletedRecords', $delRecords[dbName]);
+            }
+
+            return this;
+        },
+        isDeletedDataBase: function() {
+            return _self.getResolvers('deletedRecords').database.hasOwnProperty(dbName);
+        },
+        isDeletedTable: function(name) {
+            return _self.getResolvers('deletedRecords').table.hasOwnProperty(name);
+        },
+        reset: function() {
+            _self.register('deletedRecords', {
+                table: {},
+                database: {},
+                rename: {}
+            });
+
+            return this;
+        },
+        isExists: function() {
+            var $delRecords = getStorageItem($queryDB.$delRecordName);
+            return $delRecords && $delRecords.hasOwnProperty(dbName);
+        },
+        getRecords: function() {
+            return _self.getResolvers('deletedRecords');
+        }
     };
 };
--- src/core/resource.core.js
+++ src/core/resource.core.js
@@ -3,31 +3,63 @@
 function resourceManager(name) {
     var _resource = null,
         _resourceName = $queryDB.$dbName + "_" + name;
+
     this.getResource = function() {
         return _resource || getStorageItem(_resourceName);
     };
 
-    this.removeTableFromResource = function(tbl) {
-        var resourceControl = this.getResource();
-        if (resourceControl && resourceControl.resourceManager.hasOwnProperty(tbl)) {
-            delete resourceControl.resourceManager[tbl];
-            setStorageItem(_resourceName, resourceControl);
-        }
-    };
-
+    /**
+     * 
+     * @param {*} resource 
+     */
     this.setResource = function(resource) {
-        _resource = resource;
+        _resource = resource || _resource;
         //set and save the resource
-        setStorageItem(_resourceName, resource);
+        setStorageItem(_resourceName, _resource);
     };
 
-    this.renameTableResource = function(oldName, newName) {
-        var resourceControl = this.getResource();
-        if (resourceControl && resourceControl.resourceManager.hasOwnProperty(oldName)) {
-            resourceControl.resourceManager[newName] = resourceControl.resourceManager[oldName];
-            delete resourceControl.resourceManager[oldName];
-            resourceControl.resourceManager[newName].lastUpdated = +new Date;
-            setStorageItem(_resourceName, resourceControl);
-        }
+    this.$isExists = function() {
+        return !_resource;
+    };
+
+    this.removeResource = function() {
+        _resource = null;
+        return delStorageItem(_resourceName);
+    };
+
+    this.getTableLastSyncDate = function(tbl) {
+        return _resource && _resource.resourceManager.hasOwnProperty(tbl) && _resource.resourceManager[tbl].lastSyncedDate;
+    };
+
+    this.putTableResource = function(tbl, definition) {
+        _resource.resourceManager[tbl] = definition;
+        return this;
     }
 }
+/**
+ * 
+ * @param {*} tbl 
+ */
+resourceManager.prototype.removeTableFromResource = function(tbl) {
+    var resourceControl = this.getResource();
+    if (resourceControl && resourceControl.resourceManager.hasOwnProperty(tbl)) {
+        delete resourceControl.resourceManager[tbl];
+        this.setResource(resourceControl);
+    }
+};
+
+/**
+ * 
+ * @param {*} oldName 
+ * @param {*} newName 
+ */
+resourceManager.prototype.renameTableResource = function(oldName, newName) {
+    var resourceControl = this.getResource();
+    if (resourceControl && resourceControl.resourceManager.hasOwnProperty(oldName)) {
+        resourceControl.resourceManager[newName] = resourceControl.resourceManager[oldName];
+        delete resourceControl.resourceManager[oldName];
+        resourceControl.resourceManager[newName].lastUpdated = +new Date;
+        resourceControl.resourceManager[newName].lastSyncedDate = null;
+        this.setResource(resourceControl);
+    }
+};
